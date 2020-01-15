b7070f65c7b4639791c7219ea56593aebfe34d66
Vlad <vladnet@uw.edu>
2020-01-14 23:05:44 -0800

    Added example Config.h file

---
+++ src/Microcontroller/Config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char NETWORK[] = "network_name";
+const char PASSWORD[] = "network_password";
+const IPAddress SERVER(27, 0, 0, 1);
+const int PORT = 1234;
+
+#endif
